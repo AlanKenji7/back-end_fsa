2abdc619bb481e1cebcc14dc8e7eccf2490675e8
AlanKenji7 <alanforkenji@gmail.com>
2025-11-07 16:48:02 -0300

    Refactor config.py to improve structure and imports

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+from flask import Flask
+from flask_mail import Mail
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'uma_chave_muito_secreta_e_complexa_aqui'
@@ -32,3 +34,4 @@ config = {
     'production': ProductionConfig,
     'default': DevelopmentConfig
 }
+
